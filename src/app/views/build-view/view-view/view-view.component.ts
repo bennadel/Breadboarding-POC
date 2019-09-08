74a254595f8a209b239b87d7c07290b0f75c3d91
Ben Nadel <ben@bennadel.com>
2019-09-08 06:46:02 -0400

    I think I have the edit-view of the breadboard looking OK.

--- src/app/views/build-view/view-view/view-view.component.ts
+++ src/app/views/build-view/view-view/view-view.component.ts
@@ -1,21 +1,53 @@
 
 // Import the core angular services.
+import { ActivatedRoute } from "@angular/router";
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 // ----------------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------- //
 
 @Component({
 	selector: "poc-view-view",
+	host: {
+		"(window:keydown.a)": "gotoAdd( 'add-action' )",
+		"(window:keydown.b)": "gotoAdd( 'add-breadboard' )",
+		"(window:keydown.f)": "gotoAdd( 'add-field' )",
+		"(window:keydown.p)": "gotoAdd( 'add-placeholder' )",
+		"(window:keydown.t)": "gotoAdd( 'add-text' )",
+	},
 	styleUrls: [ "./view-view.component.less" ],
 	templateUrl: "./view-view.component.htm"
 })
 export class ViewViewComponent {
 
-	// I initialize the view component.
-	constructor() {
+	private activatedRoute: ActivatedRoute;
+	private router: Router;
 
-		// ....
+	// I initialize the view component.
+	constructor(
+		activatedRoute: ActivatedRoute,
+		router: Router
+		) {
+
+		this.activatedRoute = activatedRoute;
+		this.router = router;
+
+	}
+
+	// ---
+	// PUBLIC METHODS.
+	// ---
+
+	// I navigate to the given action segment.
+	public gotoAdd( segment: string ) : void {
+
+		this.router.navigate(
+			[ `../${ segment }` ],
+			{
+				relativeTo: this.activatedRoute
+			}
+		);
 
 	}
 
